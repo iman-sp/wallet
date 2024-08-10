4847077f4bc2667ac945bdc0cd647e1012a3712b
Iman Peyvandi Pour <iman_sp@yahoo.com>
2024-08-10 17:16:57 +0330

    chore: create wallet relation in user model, and create wallet for each user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -34,6 +35,14 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    protected static function booted(): void
+    {
+        // Create wallet for the user when it created
+        static::created(function (User $user) {
+            $user->wallet()->create();
+        });
+    }
+
     /**
      * Get the attributes that should be cast.
      *
@@ -45,5 +54,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function wallet(): HasOne
+    {
+        return $this->hasOne(Wallet::class);
     }
 }
