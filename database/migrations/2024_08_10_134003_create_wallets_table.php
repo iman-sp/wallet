3ed8c2a776daa99c0cfa98d418a4cd2de668310c
Iman Peyvandi Pour <iman_sp@yahoo.com>
2024-08-10 17:41:27 +0330

    chore: add user_id foreign key into wallet migration

--- database/migrations/2024_08_10_134003_create_wallets_table.php
+++ database/migrations/2024_08_10_134003_create_wallets_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->bigInteger('credit')->default(0);
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
     }
 
