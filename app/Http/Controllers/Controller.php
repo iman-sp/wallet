4540a10ded30da9e8fa1e6b8d603d4456b31a177
Iman Peyvandi Pour <iman_sp@yahoo.com>
2024-08-10 14:32:45 +0330

    docs: add swagger base info

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,25 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\OpenApi(
+ *  @OA\Info(
+ *      title="Wallet",
+ *      version="0.0.1",
+ *      description="API documentation for Wallet project",
+ *      @OA\Contact(
+ *          email="iman_sp@yahoo.com"
+ *      )
+ *  ),
+ *    @OA\Server(
+ *      description="Web server",
+ *      url=L5_SWAGGER_CONST_HOST
+ *  ),
+ *  @OA\PathItem(
+ *      path="/"
+ *  )
+ * ),
+ */
 abstract class Controller
 {
     //
